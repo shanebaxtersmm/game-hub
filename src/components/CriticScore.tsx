7fadbe7cf58e5fd03fe1269c52b0902c8e15ecac
shane.baxter@sea.live <shane.baxter@sea.live>
2025-03-06 14:06:45 +0000

    Get optimized images.

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -4,7 +4,7 @@ interface Props {
   score: number;
 }
 const CriticScore = ({ score }: Props) => {
-  let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+  const color = score > 75 ? "green" : score > 60 ? "yellow" : "";
   return (
     <Badge
       fontSize={"14px"}
