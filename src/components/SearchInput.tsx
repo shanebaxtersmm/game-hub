ff60de3f18c338e0cb0c4381884115ece8c6022b
shane.baxter@sea.live <shane.baxter@sea.live>
2025-03-06 17:50:19 +0000

    Build search input

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,23 @@
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { useRef } from "react";
+import { BsSearch } from "react-icons/bs";
+
+const SearchInput = () => {
+  const ref = useRef<HTMLInputElement>(null);
+
+  return (
+    <>
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />} />
+        <Input
+          ref={ref}
+          borderRadius={20}
+          placeholder="Search games..."
+          variant="filled"
+        />
+      </InputGroup>
+    </>
+  );
+};
+
+export default SearchInput;
